b60950f4c4035d3ba5d7dab400b921de2628e451
yaoyongchao <yongchao.yao@aliyun.com>
2020-01-15 18:28:56 +0800

    1、解决渲染溢出问题，

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -71,163 +71,161 @@ class _LoginPageState extends MvpBaseWidgetState<LoginPage,LoginView,LoginPresen
 
   @override
   Widget buildWidgets() {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.fromLTRB(Dimens.marginLogin, 90.0, Dimens.marginLogin, 10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Image(
-            image: AssetImage("images/ic_logo_login.png"),
-            width: 115.0,
-            height: 32.0,
-            alignment: Alignment.centerLeft,
-          ),
-          SizedBox(
-            width: 1.0,
-            height: 40.0,
-          ),
-          TextFieldClear(
-            mWidth: double.infinity,
-            mHeight: 200.0,
-            hintText: "请输入后手机号",
-            keyboardType: TextInputType.phone,
-            showBoder: true,
-            onChanged: (value) {
-              LogUtil.i("我时好人啊： " + value);
-            },
-          ),
-
-          Container(
-            decoration: BoxDecoration(
-              border:  Border(bottom: BorderSide(color: Color(MyColors.borderDefault),width: Dimens.sizeBorder)),
+    return SingleChildScrollView(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.fromLTRB(Dimens.marginLogin, 90.0, Dimens.marginLogin, 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image(
+              image: AssetImage("images/ic_logo_login.png"),
+              width: 115.0,
+              height: 32.0,
+              alignment: Alignment.centerLeft,
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: TextFieldClear(
-                    mWidth: 200.0,
-                    mHeight: 200.0,
-                    hintText: "请输入密码",
-                    keyboardType: TextInputType.number,
-                    showBoder: false,
-                    onChanged: (value) {
-                      LogUtil.i("我时好人啊： " + value);
+            SizedBox(
+              width: 1.0,
+              height: 40.0,
+            ),
+            TextFieldClear(
+              mWidth: double.infinity,
+              mHeight: 200.0,
+              hintText: "请输入后手机号",
+              keyboardType: TextInputType.phone,
+              showBoder: true,
+              onChanged: (value) {
+                LogUtil.i("我时好人啊： " + value);
+              },
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                border:  Border(bottom: BorderSide(color: Color(MyColors.borderDefault),width: Dimens.sizeBorder)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextFieldClear(
+                      mWidth: 200.0,
+                      mHeight: 200.0,
+                      hintText: "请输入密码",
+                      keyboardType: TextInputType.number,
+                      showBoder: false,
+                      onChanged: (value) {
+                        LogUtil.i("我时好人啊： " + value);
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                  ),
+                  FlatButton(
+                    child: Text("发送验证码",style:TextStyle(fontSize: 16)),
+                    padding: EdgeInsets.all(0.0),
+                    textColor: Color(MyColors.colorGey),
+                    onPressed: (){
+                      LogUtil.i("111111");
                     },
                   ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Row(
+              children: <Widget>[
+                Text("未注册的手机号,请",
+                  style: TextStyle(
+                      fontSize: 12.0
+                  ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                ),
-                FlatButton(
-                  child: Text("发送验证码",style:TextStyle(fontSize: 16)),
-                  padding: EdgeInsets.all(0.0),
-                  textColor: Color(MyColors.colorGey),
-                  onPressed: (){
-                    LogUtil.i("111111");
-                  },
-                ),
+                Text("联系客服",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color:Color(0xfff7ba53),
+                      decoration: TextDecoration.underline
+                  ),),
+                Text("注册",
+                  style: TextStyle(
+                      fontSize: 12.0
+                  ),),
               ],
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-          ),
-          Row(
-            children: <Widget>[
-              Text("未注册的手机号,请",
-                style: TextStyle(
-                  fontSize: 12.0
-              ),
-              ),
-              Text("联系客服",
-                style: TextStyle(
-                    fontSize: 12.0,
-                  color:Color(0xfff7ba53),
-                  decoration: TextDecoration.underline
-                ),),
-              Text("注册",
-                style: TextStyle(
-                    fontSize: 12.0
-                ),),
-            ],
-          ),
-          SizedBox(
-            width: 1,
-            height: 40.0,
-          ),
-          FlatButton(
-            child: Container(
-              alignment: Alignment.center,
-              child: Text("登 录",style: TextStyle(color: Colors.white,fontSize: 20.0),),
-              height: 44.0,
-              width: double.infinity,
-            ),
-//            colorBrightness: Brightness.dark,
-            hoverColor: Color(MyColors.colorBtnLoginPressed),
-            color: Color(MyColors.colorBtnLoginNormal),
-            shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(22.0)),
-            onPressed: () {
-
-            },
-          ),
-          Expanded(
-            child: SizedBox(
+            SizedBox(
               width: 1,
               height: 40.0,
             ),
-          ),
-          Container(
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image(
-                    image: AssetImage("images/ic_kf.png"),
-                    width: 13.0,
-                    height: 13.0,
-                    alignment: Alignment.centerLeft,
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  Text(
-                      "客服热线 " + Strings.customerHotline,
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      color: Color(MyColors.colorGey)
-                    ),
-                  )
-                ],
+            FlatButton(
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("登 录",style: TextStyle(color: Colors.white,fontSize: 20.0),),
+                height: 44.0,
+                width: double.infinity,
               ),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Color(0xFFF1F0F5),width: 0.5),
-                  borderRadius: BorderRadius.circular(15.0)
+//            colorBrightness: Brightness.dark,
+              hoverColor: Color(MyColors.colorBtnLoginPressed),
+              color: Color(MyColors.colorBtnLoginNormal),
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(22.0)),
+              onPressed: () {
+
+              },
+            ),
+
+            Container(
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage("images/ic_kf.png"),
+                      width: 13.0,
+                      height: 13.0,
+                      alignment: Alignment.centerLeft,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      "客服热线 " + Strings.customerHotline,
+                      style: TextStyle(
+                          fontSize: 13.0,
+                          color: Color(MyColors.colorGey)
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFF1F0F5),width: 0.5),
+                    borderRadius: BorderRadius.circular(15.0)
+
+                ),
+                alignment: Alignment.center,
+                width: 200.0,
+                height: 32.0,
 
               ),
               alignment: Alignment.center,
-              width: 200.0,
-              height: 32.0,
-
             ),
-            alignment: Alignment.center,
-          ),
-          Container(
-            height: 30.0,
-            alignment: Alignment.center,
-            child: Text(
+            Container(
+              height: 30.0,
+              alignment: Alignment.center,
+              child: Text(
                 "我已阅读并同意《用户协议》",
-              style: TextStyle(
-                fontSize: 10.0,
-                color: Color(0xFFC1C1C1)
+                style: TextStyle(
+                    fontSize: 10.0,
+                    color: Color(0xFFC1C1C1)
+                ),
               ),
+              margin: EdgeInsets.fromLTRB(0,0,0,0),
             ),
-            margin: EdgeInsets.fromLTRB(0,0,0,0),
-          ),
-        ],
+
+          ],
+        ),
       ),
     );
   }
